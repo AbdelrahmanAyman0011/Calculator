d3bb2640a3c8f93f05b644c4c40bdf583410969c
bedo <abdelrahmanayman0011@gmail.com>
2023-08-02 02:34:57 +0300

    it's my calculatour project

---
+++ Calulatour/src/javaapplication42/JavaApplication42.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication42;
+import java.awt.Color;
+
+/**
+ *
+ * @author mosta
+ */
+public class JavaApplication42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                   new calculator().setVisible(true);
+
+    }
+    
+}
